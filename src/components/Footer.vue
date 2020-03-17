66e4906c84b14ee2fb4f6d285f67c4a4a8add146
Martins Abiodun <martinsabiodun94@gmail.com>
2020-03-17 18:33:07 +0100

    Modified footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,15 @@
 <template>
    <!-- footer area start -->
-    <footer class="footer-area" style="padding-top:35px">
+    <footer class="footer-area">
         <div class="container">
             <div class="footer-top">
                 <div class="row">
-                     <div class="col-sm-12">
-                        <div class="footer-social text-sm-right" style="text-align: center!important">
-                           
                     <div class="col-sm-4">
                         <a class="footer-logo" href="#"><img src="/img/.png" alt="logo"></a>
                     </div>
                     <div class="col-sm-8">
                         <div class="footer-social text-sm-right">
                             <span>FOLLOW US</span>
-
                             <ul class="social-icon">
                                 <li>
                                     <a href="https://www.facebook.com/codingeek.net/" target="_blank"><i class="fa fa-facebook  "></i></a>
@@ -27,9 +23,6 @@
                             </ul>
                         </div>  
                     </div>
-
-                    
-                   
                 </div>
             </div>
            
